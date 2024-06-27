2aa6c5ecfef487460fdaa7f3124e9b2efcc7500f
ApplePieCodes <127224911+ApplePieCodes@users.noreply.github.com>
2024-06-27 12:13:29 -0400

    Simple Kernel

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-#![no_std]
+#![no_std] // We're on our on from here on out!
 #![no_main]
 
 use core::panic::PanicInfo;
+
+// Called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World";
+
+// No name mangleing
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize *2 + 1) = 0xb;
+        }
+    }
     loop {}
 }

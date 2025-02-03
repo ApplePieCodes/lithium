5e24e07e70cca88816080bc75e38afc3e04b1800
Liam Greenway <ApplePiePc@outlook.com>
2025-02-03 07:25:01 -0500

    WE GOT TERMINAL kjskdkjskd :3

--- src/kmain.c
+++ src/kmain.c
@@ -44,7 +44,7 @@ void kmain() {
 
     struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
 
-    struct flanterm_context *ft_ctx = flanterm_fb_init(NULL,NULL,framebuffer->address,framebuffer->width, framebuffer->height, framebuffer->pitch, framebuffer->red_mask_size, framebuffer->red_mask_shift, framebuffer->green_mask_size, framebuffer->green_mask_shift, framebuffer->blue_mask_size, framebuffer->blue_mask_shift, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 15, 15, 5, 5, 5, 0);
+    struct flanterm_context *ft_ctx = flanterm_fb_init(NULL,NULL,framebuffer->address,framebuffer->width, framebuffer->height, framebuffer->pitch, framebuffer->red_mask_size, framebuffer->red_mask_shift, framebuffer->green_mask_size, framebuffer->green_mask_shift, framebuffer->blue_mask_size, framebuffer->blue_mask_shift, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0);
 
     flanterm_write(ft_ctx, "Hello, World!\n", 15);
 
